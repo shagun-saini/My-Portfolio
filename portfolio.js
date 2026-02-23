392e4ec2cf3afe476eda76e694a9cf67ed8ba17a
shagun-saini <shagunsaini205@gmail.com>
2026-02-23 23:04:18 +0530

    Update the code and fixed error

---
+++ portfolio.js
@@ -0,0 +1,35 @@
+// 
+let home = document.querySelector("#home");
+console.log(home);
+
+homepage.onclick=()=>{
+    window.open("#home");
+    console.log("Open the Home Page");
+}
+
+
+let about = document.querySelector("#about");
+console.log(about);
+
+aboutpage.onclick=()=>{
+    window.open("#about");
+    console.log("Open the About Page");
+}
+
+
+let certificate = document.querySelector("#certificate");
+console.log(certificate);
+
+certificatepage.onclick=()=>{
+    window.open("#certificate");
+    console.log("Open the Certificate Page");
+}
+
+
+let contact = document.querySelector("#contact");
+console.log(contact);
+
+contactpage.onclick=()=>{
+    window.open("#contact");
+    console.log("Open the Contact Page");
+}
